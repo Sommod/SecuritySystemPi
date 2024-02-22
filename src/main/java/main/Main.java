c48972f50f31378590717ca4b0a66a4773b4aa4d
BuildTools <unconfigured@null.spigotmc.org>
2024-02-22 18:35:44 -0500

    Added main file that contains basic Pi4J instructions

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,70 @@
+package main;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.io.gpio.digital.impl.DefaultDigitalInputConfigBuilder;
+import com.pi4j.io.gpio.digital.impl.DefaultDigitalOutputConfigBuilder;
+import com.pi4j.platform.Platforms;
+import com.pi4j.util.Console;
+
+/**
+ * Main class of the Project
+ * @author Josh Moore
+ * @author Ben Kanter
+ *
+ */
+public class Main {
+	
+	private static int pressCount = 0;
+	private static final int PIN_BUTTON = 24; // PIN 18 = BCM 24
+	private static final int PIN_LED = 22; // Pin 15 = BCM 22
+	
+	public static void main(String[] args) {
+		Context context = Pi4J.newAutoContext();
+		
+		Platforms platforms = context.platforms();
+		Console console = new Console();
+		
+		console.box("Pi4J PLATFORMS");
+		console.println();
+		platforms.describe().print(System.out);
+		console.println();
+		
+		DigitalInputConfigBuilder ddicb = DefaultDigitalInputConfigBuilder.newInstance(context);
+		ddicb.id("button").name("Press Button").address(PIN_BUTTON).pull(PullResistance.PULL_DOWN).debounce(3000L).provider("pigpio-digital-input");
+		
+		DigitalInput button =  context.create(ddicb);
+		button.addListener(e -> {
+			if(e.state() == DigitalState.LOW) {
+				pressCount++;
+				console.println("Button was pressed for the " + pressCount + "th time");
+			}
+		});
+		
+		DigitalOutputConfigBuilder docb = DefaultDigitalOutputConfigBuilder.newInstance(context);
+		docb.id("led").name("LED Flasher").address(PIN_LED).shutdown(DigitalState.LOW).initial(DigitalState.LOW).provider("pigpio-digital-output");
+		
+		DigitalOutput led = context.create(docb);
+		
+		while(pressCount < 5) {
+			if(led.equals(DigitalState.HIGH))
+				led.low();
+			else
+				led.high();
+			try {
+				Thread.sleep(500 / (pressCount + 1));
+			} catch(InterruptedException e1) {
+				System.err.println("Error, Could not use Thread, need fix...");
+				e1.printStackTrace();
+			}
+		}
+		
+		context.shutdown();
+	}
+}
