06449298fac119e93efda29f5dd07ebd3110ab65
BuildTools <unconfigured@null.spigotmc.org>
2024-02-22 18:39:28 -0500

    Added comments to Information.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -26,8 +26,11 @@ public class Main {
 	private static final int PIN_LED = 22; // Pin 15 = BCM 22
 	
 	public static void main(String[] args) {
+		
+		// Main Class for Entire Pi4J usage. This must be the first object created.
 		Context context = Pi4J.newAutoContext();
 		
+		// Used for printing to the console.
 		Platforms platforms = context.platforms();
 		Console console = new Console();
 		
@@ -36,9 +39,12 @@ public class Main {
 		platforms.describe().print(System.out);
 		console.println();
 		
+		
+		// Create an InputConfig for button handling.
 		DigitalInputConfigBuilder ddicb = DefaultDigitalInputConfigBuilder.newInstance(context);
 		ddicb.id("button").name("Press Button").address(PIN_BUTTON).pull(PullResistance.PULL_DOWN).debounce(3000L).provider("pigpio-digital-input");
 		
+		// Create Button, Add Event Handler for button
 		DigitalInput button =  context.create(ddicb);
 		button.addListener(e -> {
 			if(e.state() == DigitalState.LOW) {
@@ -47,11 +53,14 @@ public class Main {
 			}
 		});
 		
+		// Create LED configure
 		DigitalOutputConfigBuilder docb = DefaultDigitalOutputConfigBuilder.newInstance(context);
 		docb.id("led").name("LED Flasher").address(PIN_LED).shutdown(DigitalState.LOW).initial(DigitalState.LOW).provider("pigpio-digital-output");
 		
+		// Create LED Object
 		DigitalOutput led = context.create(docb);
 		
+		// Loop for LED based on button press
 		while(pressCount < 5) {
 			if(led.equals(DigitalState.HIGH))
 				led.low();
@@ -65,6 +74,7 @@ public class Main {
 			}
 		}
 		
+		// Method to shutdown Pi4J threads. Must call at end.
 		context.shutdown();
 	}
 }
