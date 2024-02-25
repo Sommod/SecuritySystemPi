5618442a4b1bc77e7e55af350d806735628ba1c7
BuildTools <unconfigured@null.spigotmc.org>
2024-02-25 17:14:43 -0500

    Added LED (2)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -81,13 +81,24 @@ public class Main {
 		      
 		var led = context.create(ledConfig);
 		
+		ledConfig = DigitalOutput.newConfigBuilder(context)
+				.id("led_1")
+				.name("LED Dimmer")
+				.address(PIN_LED_2)
+				.shutdown(DigitalState.LOW)
+				.initial(DigitalState.LOW)
+				.provider("pigpio-digital-output");
+		var led_2 = context.create(ledConfig);
+		
 		short mode_1 = 1;
 
 		while (exitCount < 5) {
 		      if (led.equals(DigitalState.HIGH)) {
 		           led.low();
+		           led_2.low();
 		      } else {
 		           led.high();
+		           led_2.high();
 		      }
 		      try {
 				Thread.sleep(500 / (mode_1 <= 4 ? 1 : mode_1 <= 8 ? 2 : 5));
@@ -102,76 +113,6 @@ public class Main {
 				e1.printStackTrace();
 			}
 		}
-		
-//		var context = Pi4J.newAutoContext();
-//		var platforms = context.platforms();
-//		platforms.describe().print(System.out);
-//		Console console = new Console();
-//		
-//		var buttonConfig = DigitalInput.newConfigBuilder(context).id("button_1").name("Change Button Type")
-//				.address(PIN_BUTTON_1).pull(PullResistance.PULL_DOWN).debounce(3000L).provider("pigpio-digital-input");
-//		
-//		var button_1 = context.create(buttonConfig);
-//		button_1.addListener(b_1 -> {
-//			if(b_1.state() == DigitalState.LOW) {
-//				LED_CHOICE = !LED_CHOICE;
-//				exitCount = 0;
-//				console.println("Changed LED handler to " + (LED_CHOICE ? "Dimmer" : "Flasher"));
-//			}
-//		});
-//		
-//		buttonConfig = DigitalInput.newConfigBuilder(context).id("button_2").name("Change LED Property").address(PIN_BUTTON_2)
-//				.pull(PullResistance.PULL_DOWN).debounce(3000L).provider("pigpio-digital-input");
-//		
-//		var button_2 = context.create(buttonConfig);
-//		button_2.addListener(b_2 -> {
-//			if(b_2.state() == DigitalState.LOW) {
-//				if(LED_CHOICE) {
-//					pressCount++;
-//					
-//					if(pressCount > 5)
-//						pressCount = 1;
-//					
-//					console.println("## Current Light Mode: " + (pressCount) + " / 5");
-//				} else {
-//					stopBlinkChange = !stopBlinkChange;
-//					console.println("## The LED Blinker auto-change is currently set to: " + (LED_CHOICE ? "True" : "False"));
-//					exitCount++;
-//					console.println("-- Exit Count is: " + exitCount);
-//				}
-//			}
-//		});
-//		
-//		var ledConfig = DigitalOutput.newConfigBuilder(context).id("led_1").name("LED Flasher")
-//				.address(PIN_LED_1).shutdown(DigitalState.LOW).initial(DigitalState.LOW).provider("pigpio-digital-output");
-//		var led_1 = context.create(ledConfig);
-//		
-//		var ledConfig2 = DigitalOutput.newConfigBuilder(context).id("led_2").name("LED Dimmer").address(PIN_LED_2).
-//				shutdown(DigitalState.LOW).initial(DigitalState.LOW).provider("pigpio-digital-output");
-//		var led_2 = context.create(ledConfig2);
-//		
-//		short mode_1 = 1;
-//		
-//		while(exitCount < 5) {
-//			// LED 2
-//			if(led_1.equals(DigitalState.HIGH)) {
-//				led_1.low();
-//			} else {
-//				led_1.high();
-//			}
-//			
-//			try {
-//				Thread.sleep((500 / (mode_1 <= 4 ? 1 : mode_1 <= 8 ? 2 : mode_1 <= 20 ? 5 : 1)));
-//			} catch(InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-//			if(!stopBlinkChange) { mode_1++; }
-//			
-//			// LED 1
-//			led_2.pulse(pressCount, TimeUnit.SECONDS);
-//		}
 		
 		context.shutdown();
 	}
