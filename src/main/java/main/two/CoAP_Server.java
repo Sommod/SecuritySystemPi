71111132f40dc209004f05670fcf39d3c917415f
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 15:50:23 -0400

    Fixed potential errors in code

--- src/main/java/main/two/CoAP_Server.java
+++ src/main/java/main/two/CoAP_Server.java
@@ -13,53 +13,54 @@ import org.eclipse.californium.elements.config.Configuration;
 import org.eclipse.californium.elements.util.NetworkInterfacesUtil;
 
 public class CoAP_Server extends CoapServer {
-	private static final int COAP_PORT = 
-            Configuration.getStandard().get( CoapConfig.COAP_PORT );
-    private static final String tempUnti = "F";
-    float temperature = 70;
-    CoapTempServer
 
-    public static void main(String[] args) {
-        try {
-            CoapTempServer server = new CoapTempServer();
-            server.start();
-        }
-        catch ( Exception e ) {
-            System.err.println("CoAP server err: " + e.getMessage());
-        }
-    }
+	private static final int COAP_PORT = Configuration.getStandard().get(CoapConfig.COAP_PORT);
+	private static final String tempUnit = "F";
+	float temperature = 70;
 
-    public CoapTempServer() throws SocketException {
-        super();
-        addEndpoints();
-        add( new TemperatureResource() );
-    }
+	public static void main(String[] args) {
+		try {
+			CoAP_Server server = new CoAP_Server();
+			server.start();
+		} catch(Exception e) {
+			System.err.println("CoAP server err: " + e.getMessage());
+		}
+	}
 
-    private void addEndpoints() {
-        Configuration config = Configuration.getStandard();
-        // Add an endpoint listener for each host network interface
-        for (InetAddress addr : 
-             NetworkInterfacesUtil.getNetworkInterfaces()) {
-            InetSocketAddress bindToAddress = 
-                new InetSocketAddress(addr, COAP_PORT);
-            CoapEndpoint.Builder builder = new CoapEndpoint.Builder();
-            builder.setInetSocketAddress(bindToAddress);
-            builder.setConfiguration(config);
-            addEndpoint( builder.build() );
-        }
-    }
+	public CoAP_Server() throws SocketException {
+		super();
+		addEndpoints();
+		add(new TemperatureResource());
+	}
 
-    class TemperatureResource extends CoapResource {
-        public HelloWorldResource() {
-            super("temp"); // set resource URI identifier
-            getAttributes().setTitle("Server room temperature");
-        }
+	private void addEndpoints() {
+		Configuration config = Configuration.getStandard();
+		// Add an endpoint listener for each host network interface
+		for(InetAddress addr : NetworkInterfacesUtil.getNetworkInterfaces()) {
+			InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+			CoapEndpoint.Builder builder = new CoapEndpoint.Builder();
+			builder.setInetSocketAddress(bindToAddress);
+			builder.setConfiguration(config);
+			addEndpoint(builder.build());
+		}
+	}
 
-        @Override
-        public void handleGET(CoapExchange exchange) {
-            // get latest temperature reading and return it
-            temperature = …
-            exchange.respond(temperature + " degrees " + tempUnit);
-        }
-    }
+	class TemperatureResource extends CoapResource {
+
+		public TemperatureResource() {
+			super("temp");
+			getAttributes().setTitle("Server room temperature");
+		}
+		// public HelloWorldResource() {
+		// super("temp"); // set resource URI identifier
+		// getAttributes().setTitle("Server room temperature");
+		// }
+
+		@Override
+		public void handleGET(CoapExchange exchange) {
+			// get latest temperature reading and return it
+			temperature = 70F; 
+			exchange.respond(temperature + " degrees " + tempUnit);
+		}
+	}
 }
