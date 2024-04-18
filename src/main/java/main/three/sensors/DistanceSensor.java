927a8d37bb2089c62e81996c347563009e6e6fe1
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 16:38:09 -0400

    Updated Final Project classes

---
+++ src/main/java/main/three/sensors/DistanceSensor.java
@@ -0,0 +1,23 @@
+package main.three.sensors;
+
+import org.eclipse.californium.core.CoapResource;
+
+public class DistanceSensor extends CoapResource {
+	
+	private float currentDistance;
+	private float countDownTimer;
+
+	public DistanceSensor() {
+		super("distance");
+		getAttributes().setTitle("Gets the Distance Sensors value");
+		 currentDistance = -1F;
+		 countDownTimer = -2;
+	}
+	
+	public void triggerCountdown() {
+		//TODO: Need to create loop for 30s.
+		//TODO: If cancel action is given, then stop timer
+		countDownTimer = 30F;
+	}
+	
+}
--- src/main/java/main/three/sensors/NumPad.java
+++ src/main/java/main/three/sensors/NumPad.java
@@ -7,14 +7,6 @@ package main.three.sensors;
  */
 public class NumPad {
 	
-	/**
-	 * These contain the pin IDs for the NumPad as well
-	 * as methods for determining if the NumPad was
-	 * @author Josh Moore
-	 *
-	 */
-	public enum GPIO_PINS {
-		P_1, P_7, P_8, P_12, P_16, P_20, P_21, P_25;
-	}
+	//TODO: Have Python driver for NumPad
 	
 }
