c69a1afd209c62109253a67018174a8038649a03
BuildTools <unconfigured@null.spigotmc.org>
2024-04-21 16:46:43 -0400

    Added Smoke Detection

--- src/main/java/main/three/sensors/SmokerDetectorSensor.java
+++ src/main/java/main/three/sensors/SmokerDetectorSensor.java
@@ -1,10 +1,34 @@
 package main.three.sensors;
 
 import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalStateChangeEvent;
+import com.pi4j.io.gpio.digital.DigitalStateChangeListener;
+import com.pi4j.io.gpio.digital.PullResistance;
 
 public class SmokerDetectorSensor {
-
+	private DigitalInput smoker;
+	private final int SMOKE_PIN = 27;
+	
 	public SmokerDetectorSensor(Context context) {
+		DigitalInputConfigBuilder smokeBuilder = DigitalInput.newConfigBuilder(context).id("smoker").address(SMOKE_PIN)
+				.pull(PullResistance.PULL_DOWN).debounce(3000L).provider("pigpio-digitial-input");
+		
+		smoker = context.create(smokeBuilder);
+		smoker.addListener(new Temp());
+	}
+	
+	private void sendMessage() {
+		System.out.println("digital state of smoker has changed.");
+	}
+	
+	private class Temp implements DigitalStateChangeListener {
+
+		@Override
+		public void onDigitalStateChange(DigitalStateChangeEvent event) {
+			sendMessage();
+		}
 		
 	}
 }
