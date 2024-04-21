04a0c360d322dcac9af664ab8cb807a8f42f8434
BuildTools <unconfigured@null.spigotmc.org>
2024-04-21 16:29:23 -0400

    Added loop for program

---
+++ src/main/java/main/three/Manager.java
@@ -0,0 +1,68 @@
+package main.three;
+
+import com.pi4j.context.Context;
+
+import main.three.sensors.DistanceSensor;
+import main.three.sensors.NoiseController;
+import main.three.sensors.NumPad;
+import main.three.sensors.SmokerDetectorSensor;
+
+/**
+ * Main Handler class for the Final Project. This initializes all the GPIO ports on the breadboard.
+ * The 'Alarm' system is initialized with the Buzzer/LED connected to the same GPIO port, the Distance
+ * Sensor handling any change to initiate a count-down, and a Smoke detector for additional support.
+ * 
+ * @author Josh Moore
+ *
+ */
+public class Manager {
+	private DistanceSensor distanceSensor;
+	private SmokerDetectorSensor smokerDetectorSensor;
+	private NoiseController noiseController;
+	private NumPad numPad;
+	
+	private Context context;
+	private AlarmSystem alarm;
+	
+	private boolean soundAlarm;
+	private boolean exitCode;
+	
+	public Manager(Context context) {
+		this.context = context;
+		soundAlarm = false;
+		exitCode = false;
+		
+		InitializeSensors();
+		run();
+	}
+	
+	private void InitializeSensors() {
+		noiseController = new NoiseController(context);
+		distanceSensor = new DistanceSensor(context);
+		smokerDetectorSensor = new SmokerDetectorSensor(context);
+		numPad = new NumPad(context);
+		
+		alarm = new AlarmSystem();
+	}
+	
+	public void run() {
+		while(!exitCode) {
+			if(!soundAlarm) { // Check Distance Sensor / Smoker Detector.
+				//TODO: Get sensors hooked up (code-wise)
+				//TODO: If Smoke Detector is going off, send message to server.
+				//TODO: Else If distance is different (by a range)... then activate 'START' method in 'alarm'.
+				//		Check if Alarm is already running, then ignore.
+				//TODO: Else check if 'didSoundAlarm' is true, then set soundAlarm to true
+			} else { // Running the alarm system.
+				if(noiseController.isLightOn())
+					noiseController.turnLightOff();
+				else
+					noiseController.turnLightOn();
+			}
+			
+			try { Thread.sleep(500L); }
+			catch(InterruptedException e) {}
+		}
+	}
+	
+}
