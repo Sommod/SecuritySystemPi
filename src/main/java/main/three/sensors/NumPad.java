1ee58dd7affe2e678629be41bd1039f3d689b487
BuildTools <unconfigured@null.spigotmc.org>
2024-04-21 19:53:32 -0400

    Added getters for inputted code

--- src/main/java/main/three/sensors/NumPad.java
+++ src/main/java/main/three/sensors/NumPad.java
@@ -5,16 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Class for handling the NumPad interactions
+ * Handler class for the NumPad. Since Documention for the Java implementation of the NumPad
+ * is rare and difficult to find, a Python file named 'numpad.py' is executed and used as a
+ * driver for obtaining the Numpad input. This file simply obtains the Standard Input of the
+ * file and uses it to determine if the code entered is a correct one.
  * @author Josh Moore
  *
  */
 public class NumPad {
 	
+	private final String[] codes = {"123A","456B","789C"};
+	private String input;
+	private int codeEnter;
+	
 	public NumPad() {
 		String line = null;
-		String code = "123A";
-		String checker = "";
+		input = "";
+		codeEnter = -1;
+		
 		try {
 			System.out.println("Enter in code:");
 			Process p = Runtime.getRuntime().exec("python numpad.py");
@@ -22,21 +30,24 @@ public class NumPad {
 			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 			
 			while((line = stdInput.readLine()) != null) {
-				System.out.println(line);
-				checker += line;
+				System.out.print(line);
+				input += line;
 			}
 			
-			while ((line = stdError.readLine()) != null) {
-                System.out.println(line);
-            }
+			while ((line = stdError.readLine()) != null) { }
 			
-			if(checker.equals(code))
-				System.out.println("You have entered the correct code.");
-			else
-				System.out.println("Incorrect code.");
+			for(int i = 0; i < codes.length; i++) {
+				if(codes[i].equals(input)) {
+					codeEnter = i;
+					break;
+				}
+			}
 			
 			System.exit(0);
 		} catch(IOException e) {
 		}
 	}
+	
+	public int getCode() { return codeEnter; }
+	public boolean isCorrectCode() { return codeEnter != -1; }
 }
