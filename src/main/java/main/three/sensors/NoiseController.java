9ed3c02a2689ec2eccbeb2e19ec46a97863e48a1
BuildTools <unconfigured@null.spigotmc.org>
2024-04-21 16:29:59 -0400

    Removed unneeded code

--- src/main/java/main/three/sensors/NoiseController.java
+++ src/main/java/main/three/sensors/NoiseController.java
@@ -1,4 +1,4 @@
-package main.three;
+package main.three.sensors;
 
 import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalOutput;
@@ -7,15 +7,16 @@ import com.pi4j.io.gpio.digital.DigitalState;
 
 /**
  * Helper class for the LED light. This is only used in tangent with the Buzzer. As such,
- * this only contains a minimal amount of code for handling the LED.
+ * this only contains a minimal amount of code for handling the LED. This simply acts as
+ * a medium for both the buzzer and LED (This is because both will be going off at the same time).
  * @author Josh Moore
  *
  */
-public class LightController {
+public class NoiseController {
 	private static final int LED_PIN = 2;
 	private DigitalOutput led;
 	
-	public LightController(Context context) {
+	public NoiseController(Context context) {
 		DigitalOutputConfigBuilder ledConfig = DigitalOutput.newConfigBuilder(context).id("led").address(LED_PIN).shutdown(DigitalState.LOW).initial(DigitalState.LOW).provider("pigpio-digital-output");
 		led = context.create(ledConfig);
 	}
