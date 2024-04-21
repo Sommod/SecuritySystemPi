d049992a34e8fbfa2e06131216969e7af718b98d
BuildTools <unconfigured@null.spigotmc.org>
2024-04-21 15:15:10 -0400

    Fixed issue with class

--- src/main/java/main/three/sensors/BeeperSensor.java
+++ src/main/java/main/three/sensors/BeeperSensor.java
@@ -2,4 +2,10 @@ package main.three.sensors;
 
 import org.eclipse.californium.core.CoapResource;
 
-public class BeeperSensor extends CoapResource {}
+public class BeeperSensor extends CoapResource {
+
+	public BeeperSensor(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+}
